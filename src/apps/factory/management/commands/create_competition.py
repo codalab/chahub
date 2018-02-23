1546b7466fd8f4a8e8a5f4237aadf2a124eee8af
Tthomas63 <mr.tyler.thomas@gmail.com>
2018-02-23 14:19:18 -0800

    Cruft removal

--- src/apps/factory/management/commands/create_competition.py
+++ src/apps/factory/management/commands/create_competition.py
@@ -2,30 +2,16 @@ import datetime
 import random
 import uuid
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from competitions.models import Competition, Phase, Submission
-from datasets.models import DataGroup, Data
+from competitions.models import Competition
 
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
-    # def add_arguments(self, parser):
-    #     # parser.add_argument('poll_id', nargs='+', type=int)
-
     def handle(self, *args, **options):
-        # for poll_id in options['poll_id']:
-        #     try:
-        #         poll = Poll.objects.get(pk=poll_id)
-        #     except Poll.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
-        #
-        #     poll.opened = False
-        #     poll.save()
-        #
-        #     self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
         try:
             new_comp = Competition.objects.create(title="Competition {}".format(uuid.uuid4()),
                                                   created_by="User_{}".format(uuid.uuid4()), remote_id=999)
