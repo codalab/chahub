abc3bfbab4208b6aa451be58f8241b242c3e8071
Tthomas63 <mr.tyler.thomas@gmail.com>
2018-03-09 11:01:27 -0800

    Remove debug prints

--- src/apps/competitions/models.py
+++ src/apps/competitions/models.py
@@ -60,8 +60,6 @@ class Competition(models.Model):
             if phase.is_active and not phase.never_ends:
                 if phase.start and phase.end:
                     if phase.end.year >= datetime.date.today().year:
-                        print(phase.end.year)
-                        print(datetime.date.today().year)
                         return phase.end.isoformat()
         return None
 
