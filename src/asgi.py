86e7817b3a205b91462ecadf6d9ff2d416fb8e31
Eric Carmichael <eric@ckcollab.com>
2018-07-06 14:07:30 -0700

    point to new ASGI settings, back to default way

--- src/asgi.py
+++ src/asgi.py
@@ -1,5 +1,12 @@
-# import os
-# import channels.asgi
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
-# channel_layer = channels.asgi.get_channel_layer()
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
+
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+django.setup()
+application = get_default_application()
