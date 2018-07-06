3985a163e6fcabe6282fdae371dd3deaadfec211
Eric Carmichael <eric@ckcollab.com>
2018-07-06 14:16:47 -0700

    fix typo

--- src/asgi.py
+++ src/asgi.py
@@ -7,6 +7,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "setings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.base")
 django.setup()
 application = get_default_application()
