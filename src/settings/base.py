532f7d406ffa5ed48b5aa5f3b48c9cf0559396d2
Eric Carmichael <eric@ckcollab.com>
2018-07-06 14:36:48 -0700

    allow CORS all origins

--- src/settings/base.py
+++ src/settings/base.py
@@ -230,8 +230,8 @@ FILE_UPLOAD_HANDLERS = ["django.core.files.uploadhandler.TemporaryFileUploadHand
 # =============================================================================
 CORS_ORIGIN_ALLOW_ALL = True
 
-if not DEBUG and CORS_ORIGIN_ALLOW_ALL:
-    raise Exception("Disable CORS_ORIGIN_ALLOW_ALL if we're not in DEBUG mode")
+# if not DEBUG and CORS_ORIGIN_ALLOW_ALL:
+#     raise Exception("Disable CORS_ORIGIN_ALLOW_ALL if we're not in DEBUG mode")
 
 OAUTH2_PROVIDER = {
     'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
