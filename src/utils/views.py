35561ff2a5770f2d99c8a6d6286c2bce20db3ab1
Eric Carmichael <eric@ckcollab.com>
2018-08-15 11:26:21 -0700

    permission required to view producer page

--- src/utils/views.py
+++ src/utils/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 
 
@@ -9,3 +12,12 @@ class CacheMixin(object):
 
     def dispatch(self, *args, **kwargs):
         return cache_page(self.get_cache_timeout())(super(CacheMixin, self).dispatch)(*args, **kwargs)
+
+
+class AdminRequired:
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_superuser:
+            raise PermissionDenied()
+        return super().dispatch(*args, **kwargs)
