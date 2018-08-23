0b63371b8ffd0695fed2d97934c9e0349067be5f
Eric Carmichael <eric@ckcollab.com>
2018-08-23 13:10:59 -0700

    point back to proper database

--- src/settings/base.py
+++ src/settings/base.py
@@ -184,7 +184,7 @@ DATABASES = {
 }
 
 # Overridden by env settings
-db_from_env = dj_database_url.config(env="DATABASE_URL_PGBOUNCER", conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
