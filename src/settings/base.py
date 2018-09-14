567a571a99d8b44d05245c1bf63e589a27812a94
Eric Carmichael <eric@ckcollab.com>
2018-09-14 13:14:03 -0700

    more logs for chahub

--- src/settings/base.py
+++ src/settings/base.py
@@ -115,6 +115,24 @@ LOGIN_REDIRECT_URL = '/'
 
 # AUTH_USER_MODEL = 'profiles.User'
 
+# =============================================================================
+# Logging
+# =============================================================================
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
 
 # =============================================================================
 # Authentication
