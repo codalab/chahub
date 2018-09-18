ef275ea44bce6231934a439ae48196b0881210a9
Tthomas63 <mr.tyler.thomas@gmail.com>
2018-09-18 09:50:21 -0700

    Participant count sorting changes

--- src/apps/api/views/search.py
+++ src/apps/api/views/search.py
@@ -85,11 +85,17 @@ class SearchView(APIView):
     def _sort(self, search, sorting, query):
         # Make a positional list with `_score`(relevancy ranking of keyword search) as the first entry if we
         # have a valid query. Then tack on whatever field we sort by
-        sort_params = ['_score'] if query else []
+        # sort_params = ['_score'] if query else []
+        sort_params = []
         if sorting == 'participant_count':
             sort_params.append('-participant_count')
         elif sorting == 'prize':
             sort_params.append('-prize')
         elif sorting == 'deadline':
             sort_params.append('current_phase_deadline')
+
+        # If '_score' is the first sort parameter, the participant sorting gets overridden and the results are mostly
+        # relevancy based instead of being based on participant count
+        if query:
+            sort_params.append('_score')
         return search.sort(*sort_params)
