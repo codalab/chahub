27b0e9de96f1f44feb776b8bcf3625b8d8fe8d44
Eric Carmichael <eric@ckcollab.com>
2018-12-03 15:21:34 -0800

    sort default results by start date and participant count

--- src/utils/search.py
+++ src/utils/search.py
@@ -3,10 +3,10 @@ from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 
 
-def get_search_client(size=35):
+def get_search_client():
     client = Elasticsearch(settings.ELASTICSEARCH_DSL['default']['hosts'])
     s = Search(using=client)
-    s = s.extra(size=size)
+    # s = s.extra(size=size)  # default was 'size=35' -- we're displaying all competitions all the time now
     s = s.filter('term', published=True)
     s = s.source(excludes=["html_text"])
     return s
@@ -20,5 +20,6 @@ def get_results(search):
 def get_default_search_results():
     s = get_search_client()
     s = s.filter('term', published=True)
-    s = s.sort('_score', '-start')
+    s = s.extra(size=50)
+    s = s.sort('-start', '-participant_count')
     return get_results(s)
