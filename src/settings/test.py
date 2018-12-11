8b78a57e04e58577d72243e383b39a9ab5cc523e
killbot <killbot@Killbot.com>
2018-12-11 01:35:26 -0700

    Also uncommenting out STATICFILES_STORAGE

--- src/settings/test.py
+++ src/settings/test.py
@@ -1,4 +1,4 @@
 from .base import *  # noqa: F401,F403
 
 
-#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
