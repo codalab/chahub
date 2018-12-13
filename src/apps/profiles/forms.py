29b29dec4dd67e348b7c9b7c7fc9bb77f5572bd3
Tthomas63 <mr.tyler.thomas@gmail.com>
2018-12-13 15:25:57 -0800

    Add email to sign-up form

--- src/apps/profiles/forms.py
+++ src/apps/profiles/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 from django.contrib.auth import get_user_model
+from django.forms import EmailField
 
 User = get_user_model()
 
@@ -8,5 +9,8 @@ User = get_user_model()
 class ChahubCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("username",)
-        field_classes = {'username': UsernameField}
+        fields = ("username", "email")
+        field_classes = {
+            'username': UsernameField,
+            'email': EmailField
+        }
