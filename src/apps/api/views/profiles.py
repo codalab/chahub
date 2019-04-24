232401e3c080532628fe48a68c2a254defe16d52
Tthomas63 <mr.tyler.thomas@gmail.com>
2019-04-24 13:32:25 -0700

    Add update method to profiles viewset to handle bulk update or create through PUT

--- src/apps/api/views/profiles.py
+++ src/apps/api/views/profiles.py
@@ -27,11 +27,43 @@ class ProfileViewSet(ModelViewSet):
     serializer_class = ProfileSerializer
     authentication_classes = (ProducerAuthentication, SessionAuthentication, )
     permission_classes = (ProducerPermission, )
+    http_method_names = ['GET', 'POST', 'PUT']
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
         context['producer'] = self.request.user
         return context
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        for profile in request.data:
+            try:
+                instance = self.queryset.get(remote_id=profile.get('remote_id'))
+            except Profile.DoesNotExist:
+                instance = None
+            # instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            if instance:
+                self.perform_update(serializer)
+            else:
+                self.perform_create(serializer)
+
+            if getattr(instance, '_prefetched_objects_cache', None):
+                # If 'prefetch_related' has been applied to a queryset, we need to
+                # forcibly invalidate the prefetch cache on the instance.
+                instance._prefetched_objects_cache = {}
+
+        return Response({}, status=status.HTTP_202_ACCEPTED)
+
+    # def perform_update(self, serializer):
+    #     super().perform_update()
+    #     # serializer.save()
+
+    def partial_update(self, request, *args, **kwargs):
+        # kwargs['partial'] = True
+        # return self.update(request, *args, **kwargs)
+        return super().partial_update(request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
         """Overriding this for the following reasons:
