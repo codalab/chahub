6a52ed4858cb26db1f774d52e7bccd32f8445665
Tthomas63 <mr.tyler.thomas@gmail.com>
2019-04-24 13:49:42 -0700

    Modify create to handle updating existing objects based on a unique field

--- src/apps/api/views/profiles.py
+++ src/apps/api/views/profiles.py
@@ -27,49 +27,6 @@ class ProfileViewSet(ModelViewSet):
     serializer_class = ProfileSerializer
     authentication_classes = (ProducerAuthentication, SessionAuthentication, )
     permission_classes = (ProducerPermission, )
-    http_method_names = ['GET', 'POST', 'PUT']
-
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context['producer'] = self.request.user
-        return context
-
-    # def dispatch(self, request, *args, **kwargs):
-    #     super().dispatch()
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        for profile in request.data:
-            try:
-                instance = self.queryset.get(remote_id=profile.get('remote_id'))
-            except Profile.DoesNotExist:
-                instance = None
-            # instance = self.get_object()
-            serializer = self.get_serializer(instance, data=request.data, partial=partial)
-            serializer.is_valid(raise_exception=True)
-            if instance:
-                self.perform_update(serializer)
-            else:
-                self.perform_create(serializer)
-
-            if getattr(instance, '_prefetched_objects_cache', None):
-                # If 'prefetch_related' has been applied to a queryset, we need to
-                # forcibly invalidate the prefetch cache on the instance.
-                instance._prefetched_objects_cache = {}
-
-        return Response({}, status=status.HTTP_202_ACCEPTED)
-
-    # def perform_update(self, serializer):
-    #     super().perform_update()
-    #     # serializer.save()
-
-    def partial_update(self, request, *args, **kwargs):
-        # kwargs['partial'] = True
-        # return self.update(request, *args, **kwargs)
-        return super().partial_update(request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
         """Overriding this for the following reasons:
@@ -79,7 +36,19 @@ class ProfileViewSet(ModelViewSet):
            custom to make drf-writable-nested able to interpret everything easily"""
         # Make the serializer take many competitions at once
         for profile in request.data:
-            serializer = self.get_serializer(data=profile)
+            # serializer = self.get_serializer(data=profile)
+            # serializer.is_valid(raise_exception=True)
+            # self.perform_create(serializer)
+
+            try:
+                instance = self.queryset.get(remote_id=profile.get('remote_id'))
+            except Profile.DoesNotExist:
+                instance = None
+            serializer = self.get_serializer(instance, data=request.data, partial=False)
             serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
+            if instance:
+                self.perform_update(serializer)
+            else:
+                self.perform_create(serializer)
+
         return Response({}, status=status.HTTP_201_CREATED)
