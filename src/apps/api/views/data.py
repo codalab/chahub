68e4b91b8a2cffc0e35843fe609b9367208021a7
Tthomas63 <mr.tyler.thomas@gmail.com>
2019-04-30 14:47:47 -0700

    Abstract bulk viewset logic to BulkViewSetMixin

--- src/apps/api/views/data.py
+++ src/apps/api/views/data.py
@@ -8,6 +8,7 @@ from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from api.authenticators import ProducerAuthentication
 from api.permissions import ProducerPermission
 from api.serializers import data as serializers
+from api.views.mixins import BulkViewSetMixin
 from datasets.models import Data, DataGroup
 
 
@@ -59,7 +60,7 @@ from datasets.models import Data, DataGroup
 #     permission_classes = (IsAuthenticated,)
 
 # NOTE: We don't have delete mixin
-class DataViewSet(ModelViewSet):
+class DataViewSet(BulkViewSetMixin, ModelViewSet):
     """Updating and inserting competitions are done by Producers.
 
     request.user = Producer in this case."""
@@ -68,34 +69,34 @@ class DataViewSet(ModelViewSet):
     authentication_classes = (ProducerAuthentication,)
     permission_classes = (ProducerPermission,)
 
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context['producer'] = self.request.user
-        return context
-
-    def get_queryset(self):
-        qs = self.queryset
-        qs = qs.prefetch_related('user', 'producer')
-
-        producer = self.request.query_params.get('producer', None)
-        creator_id = self.request.query_params.get('creator_id', None)
-
-        if producer:
-            qs = qs.filter(producer__id=producer)
-        if creator_id:
-            qs = qs.filter(creator_id=creator_id)
-
-        return qs
-
-    def create(self, request, *args, **kwargs):
-        """Overriding this for the following reasons:
-
-        1. Returning the huge amount of HTML/etc. back by default by DRF was bad
-        2. We want to handle creating many competitions this way, and we do that
-           custom to make drf-writable-nested able to interpret everything easily"""
-        # Make the serializer take many competitions at once
-        for data in request.data:
-            serializer = self.get_serializer(data=data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-        return Response({}, status=status.HTTP_201_CREATED)
+    # def get_serializer_context(self):
+    #     context = super().get_serializer_context()
+    #     context['producer'] = self.request.user
+    #     return context
+    #
+    # def get_queryset(self):
+    #     qs = self.queryset
+    #     qs = qs.prefetch_related('user', 'producer')
+    #
+    #     producer = self.request.query_params.get('producer', None)
+    #     creator_id = self.request.query_params.get('creator_id', None)
+    #
+    #     if producer:
+    #         qs = qs.filter(producer__id=producer)
+    #     if creator_id:
+    #         qs = qs.filter(creator_id=creator_id)
+    #
+    #     return qs
+    #
+    # def create(self, request, *args, **kwargs):
+    #     """Overriding this for the following reasons:
+    #
+    #     1. Returning the huge amount of HTML/etc. back by default by DRF was bad
+    #     2. We want to handle creating many competitions this way, and we do that
+    #        custom to make drf-writable-nested able to interpret everything easily"""
+    #     # Make the serializer take many competitions at once
+    #     for data in request.data:
+    #         serializer = self.get_serializer(data=data)
+    #         serializer.is_valid(raise_exception=True)
+    #         self.perform_create(serializer)
+    #     return Response({}, status=status.HTTP_201_CREATED)
---
+++ src/apps/api/views/mixins.py
@@ -0,0 +1,38 @@
+# NOTE: We don't have delete mixin
+from rest_framework import status
+from rest_framework.response import Response
+
+
+class BulkViewSetMixin(object):
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['producer'] = self.request.user
+        return context
+
+    def get_queryset(self):
+        qs = self.queryset
+        qs = qs.prefetch_related('phases', 'producer', 'admins', 'participants')
+
+        producer = self.request.query_params.get('producer', None)
+        creator_id = self.request.query_params.get('creator_id', None)
+
+        if producer:
+            qs = qs.filter(producer__id=producer)
+        if creator_id:
+            qs = qs.filter(creator_id=creator_id)
+
+        return qs
+
+    def create(self, request, *args, **kwargs):
+        """Overriding this for the following reasons:
+
+        1. Returning the huge amount of HTML/etc. back by default by DRF was bad
+        2. We want to handle creating many competitions this way, and we do that
+           custom to make drf-writable-nested able to interpret everything easily"""
+        # Make the serializer take many competitions at once
+        for object in request.data:
+            serializer = self.get_serializer(data=object)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+        return Response({}, status=status.HTTP_201_CREATED)
