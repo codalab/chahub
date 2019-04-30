a79fb5fecd5c83abbdb08e25ffb9892904eb0ebc
Tthomas63 <mr.tyler.thomas@gmail.com>
2019-04-30 14:29:13 -0700

    .Meta for Meta instead of ._meta

--- src/apps/api/serializers/mixins.py
+++ src/apps/api/serializers/mixins.py
@@ -17,12 +17,12 @@ class BulkSerializerMixin(object):
         try:
             # If we have an existing instance from this producer
             # with the same remote_id, update it instead of making a new one
-            instance = self._meta.model.objects.get(
+            instance = self.Meta.model.objects.get(
                 remote_id=validated_data.get('remote_id'),
                 producer=validated_data.get('producer')
             )
             return self.update(instance, validated_data)
-        except self._meta.model.DoesNotExist:
+        except self.Meta.model.DoesNotExist:
             new_instance = super().create(validated_data)
             return new_instance
 
