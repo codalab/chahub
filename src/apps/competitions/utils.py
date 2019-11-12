1d616e2e3cdeee4fa926424d1b1eec1974390087
Tthomas63 <mr.tyler.thomas@gmail.com>
2019-11-12 15:13:12 -0800

    Fix indentation

--- src/apps/competitions/utils.py
+++ src/apps/competitions/utils.py
@@ -31,22 +31,22 @@ def competition_download_image(competition_pk):
     )
     # TODO this is only saving PNGs, and isn't actually saving them to the competition.
     # if content_type.lower() == 'image/png':
-        logger.info("Image found")
-        image = Image.open(BytesIO(resp.content))
-        width, height = image.size
-        aspect = width / height
-        image_format = image.format
-        unsimplified_ratio = "{0}/{1}".format(width, height)
-        logger.info("Aspect ratio is: {0}, or: {1} unsimplified".format(aspect, unsimplified_ratio))
-        new_width = settings.LOGO_BASE_WIDTH
-        new_height = int(round(new_width * aspect))
-        logger.info("New Width: {0}, New Height: {1}".format(new_width, new_height))
-        image_rs = image.resize((new_width, new_height), Image.ANTIALIAS)
-        thumb_io = BytesIO()
-        image_rs.save(thumb_io, format=image_format)
-        new_image = ContentFile(thumb_io.getvalue())
-        comp.logo.save('logo_{0}.{1}'.format(comp.pk, image_format.lower()), new_image)
-        comp.save()
-        logger.info("Image file saved for competition {}".format(comp.pk))
+    logger.info("Image found")
+    image = Image.open(BytesIO(resp.content))
+    width, height = image.size
+    aspect = width / height
+    image_format = image.format
+    unsimplified_ratio = "{0}/{1}".format(width, height)
+    logger.info("Aspect ratio is: {0}, or: {1} unsimplified".format(aspect, unsimplified_ratio))
+    new_width = settings.LOGO_BASE_WIDTH
+    new_height = int(round(new_width * aspect))
+    logger.info("New Width: {0}, New Height: {1}".format(new_width, new_height))
+    image_rs = image.resize((new_width, new_height), Image.ANTIALIAS)
+    thumb_io = BytesIO()
+    image_rs.save(thumb_io, format=image_format)
+    new_image = ContentFile(thumb_io.getvalue())
+    comp.logo.save('logo_{0}.{1}'.format(comp.pk, image_format.lower()), new_image)
+    comp.save()
+    logger.info("Image file saved for competition {}".format(comp.pk))
     # else:
     #     logger.warning("Image not found")
